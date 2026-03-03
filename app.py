b91bb928dd506705d692783a932be6644a95bd52
choobipanda <atn26@humboldt.edu>
2026-03-02 20:12:08 -0800

    define request model

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class PromptRequest(BaseModel):
+    prompt: str
 
 @app.get("/")
 def root():
     return {"message": "API is running"}
+
+@app.post("/hello")
+def hello(req: PromptRequest):
+    return {"input": req.prompt}
